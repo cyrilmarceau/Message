b2a136415442ce928656e377ff8ad4284c1c1cfd
cyrilmarceau <marceau.cyril@hotmail.fr>
2022-08-02 10:34:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import Message from './components/Message.vue'
+
+
+const isSend = ref(false)
+
+const sendMessage = () => {
+  isSend.value = true
+}
+
+</script>
+
+<template>
+
+  <div>
+
+    <Message />
+    <Message message="Props de test" />
+
+
+    <form action="">
+        <textarea name="" id="" cols="30" rows="10">
+          {{ isSend }}
+        </textarea>
+    </form>
+
+     <button @click="sendMessage">Envoyer le message</button>
+  </div>
+
+  
+</template>
+
+<style scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const props = defineProps({
+  message: String
+})
+
+
+const message = ref('')
+
+
+onMounted(() => {
+    if(props.message !== undefined) {
+        message.value = props.message
+    }
+})
+</script>
+
+<template>
+  <p>{{message}}</p>
+</template>
+
+<style scoped>
+
+</style>
